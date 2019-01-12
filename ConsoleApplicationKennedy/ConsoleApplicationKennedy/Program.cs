d2b634ff838ceeafba3d38603f6c324329e556e2
waabbajak <waabbajak@gmail.com>
2019-01-12 15:28:20 -0700

    qasdfasdf

--- ConsoleApplicationKennedy/ConsoleApplicationKennedy/Program.cs
+++ ConsoleApplicationKennedy/ConsoleApplicationKennedy/Program.cs
@@ -10,6 +10,11 @@ namespace ConsoleApplicationKennedy
     {
         static void Main(string[] args)
         {
+            // welcome message
+            Console.WriteLine("");
+            Console.WriteLine("----------------------------------------------------");
+            Console.WriteLine("- Demonstration of Storing / Displaying User Input - ");
+            Console.WriteLine("----------------------------------------------------");
             // variable to store my full name
             string nameString = "";
           
@@ -26,6 +31,11 @@ namespace ConsoleApplicationKennedy
             Console.WriteLine("Your Name is: " + nameString);
             Console.WriteLine("You are from: " + placeString);
 
+            // welcome message
+            Console.WriteLine("");
+            Console.WriteLine("------------------------------------------------------");
+            Console.WriteLine("- Demonstration of Current Date but not Current Time -");
+            Console.WriteLine("------------------------------------------------------");
             // variable containing today's date
             DateTime today = DateTime.Now;
 
@@ -33,20 +43,67 @@ namespace ConsoleApplicationKennedy
             Console.Write("Today's Date: ");
             Console.WriteLine(today.ToString("d"));
 
+
+            // welcome message
+            Console.WriteLine("");
+            Console.WriteLine("------------------------------------------");
+            Console.WriteLine("- Demonstration of Christmas Calculation -");
+            Console.WriteLine("------------------------------------------");
             // variable: Christmas 2019 DateTime
             DateTime christmas = new DateTime(2019, 12, 25);
 
             // compute days till christmas
             TimeSpan daystill = christmas.Subtract(today);
-                      
-            // output number of days till christmas with label
-            Console.Write("Days Till Christmas: ");
-            Console.WriteLine(daystill.TotalDays);
 
+            // make sure christmas has not passed
+            if (daystill.TotalDays < 0)
+            {
+                Console.WriteLine("Christmas has passed this year");
+                christmas = christmas.AddYears(1);
+                Console.WriteLine("Next Christmas is " + christmas.ToString("d"));
+                TimeSpan newyear = christmas.Subtract(today);
+                Console.WriteLine("Days Till Christmas: " + newyear.TotalDays);
+            }
+            else
+            {
+                Console.WriteLine("Days Till Christmas: " + daystill.TotalDays);
+            }
+
+            // for testing purposes
+            Console.WriteLine("");
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("- Demonstration of Error Handling -");
+            Console.WriteLine("-----------------------------------");
+
+            // new day, specifically after christmas 2019
+            DateTime testday = new DateTime(2019, 12, 28);
+
+            TimeSpan teststill = christmas.Subtract(testday);
+
+            Console.WriteLine("Date After Christmas: " + testday.ToString("d"));
+            Console.WriteLine("If negative, add a year to christmas variable: " + teststill.TotalDays);
+
+            if (teststill.TotalDays < 0)
+            {
+                Console.WriteLine("Christmas has passed this year");
+                christmas = christmas.AddYears(1);
+                Console.WriteLine("Next Christmas is " + christmas.ToString("d"));
+                TimeSpan newyear = christmas.Subtract(testday);
+                Console.WriteLine("Days Till Christmas: " + newyear.TotalDays);
+            }
+            else
+            {
+                TimeSpan dayTest = christmas.Subtract(testday);
+                Console.WriteLine("TEST: " + dayTest.TotalDays);
+                Console.WriteLine(dayTest.TotalDays);
+            }
+           
             // program example from section 2.1 from the book
             Console.WriteLine("");
-            Console.WriteLine("Section From the Book");
-            Console.WriteLine("--------------------------------------------------");
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine("- Stuff From the Book - Enhanced -");
+            Console.WriteLine("----------------------------------");
+
             double width, height, woodLength, glassArea;
             string widthString, heightString;
 
